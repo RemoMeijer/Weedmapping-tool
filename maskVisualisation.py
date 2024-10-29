b6fdab330f3138900eff462ca805285d48785a6c
remco <remcomeijer0210@gmail.com>
2024-10-29 11:25:59 +0100

    Init commit

---
+++ maskVisualisation.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from random import randint
+
+with open('./annotations/bonirob_2016-05-23-10-57-33_4_frame124.txt', 'r') as f:
+    labels = f.read().splitlines()
+img = cv2.imread('./images/bonirob_2016-05-23-10-57-33_4_frame124.png')
+h, w = img.shape[:2]
+
+for label in labels:
+    class_id, *poly = label.split(' ')
+
+    poly = np.asarray(poly, dtype=np.float16).reshape(-1, 2)  # Read poly, reshape
+    poly *= [w, h]  # Unscale
+
+    cv2.polylines(img, [poly.astype('int')], True, (randint(0, 255), randint(0, 255), randint(0, 255)),
+                  2)  # Draw Poly Lines
+
+    cv2.imshow('img with poly', img)
+    cv2.waitKey(0)
